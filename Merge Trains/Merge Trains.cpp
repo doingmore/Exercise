69d06462d1d36facf6cecb791489792814db9f98
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-11-08 11:22:07 +0200

    Update Merge Trains.cpp

--- Merge Trains/Merge Trains.cpp
+++ Merge Trains/Merge Trains.cpp
@@ -9,8 +9,8 @@
 using namespace std;
 
 /*
-At a certain train station, trains arrive on two tracks – Track A and Track B – and are merged onto a single track. 
-Each railcar has a number, and in each train the railcars are ordered by number – the front railcar has the lowest number,
+At a certain train station, trains arrive on two tracks â€“ Track A and Track B â€“ and are merged onto a single track. 
+Each railcar has a number, and in each train the railcars are ordered by number â€“ the front railcar has the lowest number,
 the back railcar has the highest number. Given the railcar numbers on Track A and Track B, given from the back car to the front car,
 output the order in which railcars from Track A and Track B should be merged into the single track, then output the new train 
 configuration, starting from the last railcar, and finishing at the first railcar. Railcars are moved starting from the front
@@ -18,7 +18,7 @@ to the back (i.e. you can only move the front car of a train). Each time you mov
 it pushes forward any railcars that are already there. The first line of the console will contain the numbers of the railcars on Track A, from 
 the last railcar to the first railcar. The second line of the console will contain the numbers of the railcars on Track B, from the last railcar
 to the first railcar.If a railcar from Track A should be moved to the single track, print A. Otherwise, print B. On a separate line print the final
-configuration of the train (numbers separated by spaces, representing the railcar numbers from the last railcar to the first). The “input” railcars will
+configuration of the train (numbers separated by spaces, representing the railcar numbers from the last railcar to the first). The â€œinputâ€ railcars will
 always be correctly ordered (i.e. will be a line of descending positive integer numbers). The input will be such that the result will 
 never have any railcars with the same numbers.
 */
@@ -27,7 +27,6 @@ void read(vector<pair<int, char>>& myvec, char c) {
     string line;
     getline(cin, line);
     stringstream str(line);
-    pair<int, char> kur;
     int n;
     while (str >> n)
     {
