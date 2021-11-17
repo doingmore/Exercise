509411f64dc9753fb883e3bf8c492da2fdf748a6
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-11-17 12:02:02 +0200

    Add files via upload

---
+++ Sort Numbers SET Simple/Sort Numbers.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+/*
+Read a list of decimal numbers and sort them in increasing order. Print the output as shown in the examples below.
+*/
+
+int main()
+{
+    string input;
+    getline(cin, input);
+    stringstream stream(input);
+    set<int> listNumbers;
+    int number{ 0 };
+
+    while (stream >> number)
+    {
+        listNumbers.insert(number);
+    }
+
+    auto stop = listNumbers.end();
+    stop--;
+
+    std::set<int>::iterator it;
+    for ( auto it = listNumbers.begin(); it != listNumbers.end(); it++)
+    {
+        if (it == stop)
+        {
+            cout << *it;
+            break;
+        }
+        cout << *it<< " < ";
+    }
+
+    return 0;
+}
