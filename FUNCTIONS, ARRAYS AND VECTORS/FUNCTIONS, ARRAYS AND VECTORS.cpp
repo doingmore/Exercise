66cfbbda62aeda9727c83b19e8c95beca5cc65f4
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-11-28 20:04:55 +0200

    Add files via upload

---
+++ FUNCTIONS, ARRAYS AND VECTORS/FUNCTIONS, ARRAYS AND VECTORS.cpp
@@ -0,0 +1,11 @@
+extern void Problem1(); //Compare Arrays
+extern void Problem2(); //Longest Sequence
+extern void Problem3(); //Above Average
+extern void Problem4(); //Most Frequent Number
+extern void Problem5(); //Closest Numbers
+
+int main()
+{
+    Problem4();  // For other problem from this exercise, just change the number of problem.
+    return 0;
+}
---
+++ FUNCTIONS, ARRAYS AND VECTORS/Problem5.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+/*
+Write a program which finds the two closest (by value) integer numbers
+in an vector and prints the absolute difference between them.
+*/
+
+void Problem5()
+{
+    vector<int> numbers;
+    int length{ 0 };
+    cin >> length;
+    int symbols{ 0 };
+
+    for (int i = 0; i < length; i++)
+    {
+        cin >> symbols;
+        numbers.push_back(symbols);
+    }
+
+    int actualDifference{ 0 };
+    int closestNumber{ INT_MAX };
+    int closestNumberFirst{ 0 };
+    int closestNumberSecond{ 0 };
+
+    for (unsigned int i = 1; i < numbers.size(); i++)
+    {
+        actualDifference = 0;
+        if (numbers[i - 1] > numbers[i])
+        {
+            actualDifference = abs(numbers[i] - numbers[i - 1]);
+        }
+
+        else if (numbers[i - 1] < numbers[i])
+        {
+            actualDifference = abs(numbers[i - 1] - numbers[i]);
+        }
+        else
+        {
+            actualDifference = numbers[i - 1];
+        }
+        if (actualDifference < closestNumber)
+        {
+            closestNumber = actualDifference;
+            closestNumberFirst = numbers[i - 1];
+            closestNumberSecond = numbers[i];
+        }
+
+    }
+    for (int i = 0; i < 1; i++)
+    {
+        actualDifference = 0;
+        if (numbers[i] > numbers[length - 1])
+        {
+            actualDifference = abs(numbers[i] - numbers[length - 1]);
+        }
+
+        else if (numbers[i] < numbers[length - 1])
+        {
+            actualDifference = abs(numbers[i] - numbers[length - 1]);
+        }
+        else
+        {
+            actualDifference = numbers[i];
+        }
+        if (actualDifference < closestNumber)
+        {
+            closestNumber = actualDifference;
+            closestNumberFirst = numbers[i];
+            closestNumberSecond = numbers[length - 1];
+        }
+
+    }
+    if (closestNumber == 1)
+    {
+        cout << "All numbers in this vector are exactly 1 unit apart" << '\n';
+    }
+    else
+    {
+        cout << " The closest numbers are " << closestNumberFirst << " and " << closestNumberSecond << " absolute difference is: " << closestNumber << '\n';
+    }
+}
