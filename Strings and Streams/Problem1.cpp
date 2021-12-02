cd8b81f9ecd64a3dc17642197e520eb8da874785
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-02 11:41:13 +0200

    Add files via upload

---
+++ Strings and Streams/Problem1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void Problem1()
+{
+	cout << "Work problem 1";
+}
---
+++ Strings and Streams/Problem2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+Write a program that reads a line from the console containing a mathematical expression.
+Write a bool function that checks whether the brackets in the expression () are placed
+correctly (assume everything else is correct, i.e. you don’t need to check for correct
+signs, correct variables/numbers, etc.) and returns true if they are correct and false
+if they are not correct. Print correct or incorrect accordingly on the console
+*/
+
+bool expresionCheck(string localexprecion)
+{
+	int countOpenBrackets{ 0 };
+	int countCloseBrackets{ 0 };
+	for (unsigned int i = 0; i < localexprecion.size(); i++)
+	{
+		if (localexprecion[i] == '(') // ASCII The char '(' is equal integer value 40 
+		{
+			countOpenBrackets++;
+		}
+		if (localexprecion[i] == ')')
+		{
+			countCloseBrackets++;
+		}
+	}
+	if (countOpenBrackets == countCloseBrackets)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Problem2()
+{
+	string exprecion;
+	getline(cin, exprecion);
+
+	bool flag;
+	flag = expresionCheck(exprecion);
+
+	if (flag)
+	{
+		cout << "The expresion is with correct brackets syntax";
+	}
+	else
+	{
+		cout << "Not correct brackets syntax";
+	}
+}
