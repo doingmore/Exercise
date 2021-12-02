47924be7ef65d924d1291db49bc4f51368e6beba
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-02 11:19:47 +0200

    Add files via upload

--- Last Exercise Fundamental OPP/Last Exercise Fundamental OPP.cpp
+++ Last Exercise Fundamental OPP/Last Exercise Fundamental OPP.cpp
@@ -2,9 +2,11 @@ extern void Problem1();
 extern void Problem2();
 extern void Problem3();
 extern void Problem4();
+extern void MyProblem3();
+extern void MyProblem4();
 
 int main()
 {
-    Problem1();  // For other problem from this exercise, just change the number of problem.
+    MyProblem4();  // For other problem from this exercise, just change the number of problem.
     return 0;
 }
