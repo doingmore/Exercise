3fad79fe4655d30b2b8051e2a48a286bbcbf4926
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-06 14:11:30 +0200

    Add files via upload

---
+++ Linear Containers/Linear Containers.cpp
@@ -0,0 +1,10 @@
+extern void Problem1(); //not solvd fot the moment
+extern void Problem2(); //Positions Of Number in Matrix
+extern void Problem3(); //Merge Train
+extern void Problem4(); //Brackets
+
+int main()
+{
+    Problem2();  // for another problem just change the number of problem
+    return 0;
+}
