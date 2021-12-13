9fbebe82653ae5efb8deded19741b2237b8f66c0
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-13 12:21:44 +0200

    Add files via upload

---
+++ Header files,Pointers,References/Profits1/ProfitCalculator.h
@@ -0,0 +1,8 @@
+#ifndef ProfitCalculator_h
+
+#include "Company.h"
+
+void getProfitReport(std::vector<Company> *list, std::map<int, int> *iDTax);
+
+#define ProfitCalculator_h
+#endif // !ProfitCalculator_h
