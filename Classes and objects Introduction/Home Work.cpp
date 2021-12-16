28fa85f2e472cbd5865b55ea20aef622971df392
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-16 14:08:40 +0200

    Add files via upload

---
+++ Classes and objects Introduction/Home Work.cpp
@@ -0,0 +1,12 @@
+
+
+
+extern  void Problem1();  //Sentence Shifter-NOT SOLVED
+extern  void Problem2();  //Distance
+extern  void Problem3();  //Sales
+
+int main()
+{
+   Problem2();  // For other problem, just change the number
+    return 0;
+}
