6e0df98bf3b73dd84582c2c1474802c2b4b2d82e
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-17 18:00:03 +0200

    Delete Header files,Pointers,References directory

--- Header files,Pointers,References/Profits1/ProfitCalculator.h
+++
@@ -1,8 +0,0 @@
-#ifndef ProfitCalculator_h
-
-#include "Company.h"
-
-void getProfitReport(std::vector<Company> *list, std::map<int, int> *iDTax);
-
-#define ProfitCalculator_h
-#endif // !ProfitCalculator_h
