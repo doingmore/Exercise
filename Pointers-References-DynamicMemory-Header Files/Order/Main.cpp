b1ccb20991ba6f0165efa9565400d056afedf9eb
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-28 16:42:56 +0200

    Update Main.cpp

--- Pointers-References-DynamicMemory-Header Files/Order/Main.cpp
+++ Pointers-References-DynamicMemory-Header Files/Order/Main.cpp
@@ -5,35 +5,41 @@
 #include "Company.h"
 #include "OrderedInserter.h"
 
-using namespace std;
+std::vector<OrderedInserter> insert(std::map<int, std::string>& orderedID)
+{
+	std::vector<OrderedInserter> listCompanies;
+
+	std::map<int, std::string>::iterator it;
+	for (it = orderedID.begin(); it != orderedID.end(); it++)
+	{
+		listCompanies.push_back(OrderedInserter(it->second, it->first));
+	}
+
+	return listCompanies;
+}
 
 int main()
 {
-	vector<OrderedInserter> listCompanys;
-	map<int,string> orderedID; //saving ID in increase order
-	string inputName;
+	//std::vector<OrderedInserter> listCompanys;
+	std::map<int,std::string> ordered; //saving ID in increase order
+	std::string inputName;
 	int inputID;
 
 	//input info in map 
 	while (inputName != "end")
 	{
-		cin >> inputName;
+		std::cin >> inputName;
 		if (inputName =="end")
 		{
 			break;
 		}
-		cin >> inputID;
-		orderedID.insert(pair<int,string>(inputID,inputName));
+		std::cin >> inputID;
+		ordered.insert(std::pair<int,std::string>(inputID,inputName));
 	}
 
-	//input info from ordered map to vector
-	map<int, string>::iterator it;
-	for ( it = orderedID.begin(); it != orderedID.end(); it++)
-	{
-		listCompanys.push_back(OrderedInserter(it->second, it->first));
-	}
+	std::vector<OrderedInserter> listCompanies = insert(ordered);  //input info from ordered map to vector
 
-	getInfoAllCompanys(&listCompanys);
+	printInfoAllCompanies(listCompanies);
 
 	return 0;
 }
