7fadabb1dc168ee5608bc0d89b786ec887e31207
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-28 16:45:55 +0200

    Update ProfitsMain.cpp

--- Pointers-References-DynamicMemory-Header Files/Profits1/ProfitsMain.cpp
+++ Pointers-References-DynamicMemory-Header Files/Profits1/ProfitsMain.cpp
@@ -5,15 +5,13 @@
 #include "Company.h"
 #include "ProfitCalculator.h"
 
-using namespace std;
-
 int main()
 {
-    vector<Company> list;
-    map<int, int> iDTax;
+    std::vector<Company> list;
+    std::map<int, int> iDTax;
     
     // for member                       
-    string inputName;                   //int p = 5; // int variable
+    std::string inputName;              //int p = 5; // int variable
     int iD{ 0 };                        //int* pPointer=&p ;// pointer to int
     int revenue{ 0 };                   //int*& pPointerRef = pPointer; // reference to pointer to int
     int costs{ 0 };
@@ -23,28 +21,28 @@ int main()
 
     for (int i = 0; i < 10; i++)
     {
-        cin >> inputName;
+        std::cin >> inputName;
         if (inputName == "end")
         {
             inputName = "GO";
             break;
         }
-        cin >> iD >> revenue >> costs;
+        std::cin >> iD >> revenue >> costs;
         list.push_back(Company(inputName, iD, revenue, costs));
     }
     while (inputName != "end")
     {
-        cin >> inputName;
+        std::cin >> inputName;
         if (inputName == "end")
         {
             break;
         }
+        std::cin>>taxPrecentage;
         iD = stoi(inputName); // to int because must put in map. After that in string, because must do check for end
-        cin >> taxPrecentage;
-        iDTax.insert(pair<int, int>(iD, taxPrecentage));
+        iDTax.insert(std::pair<int, int>(iD, taxPrecentage));
     }
 
-    getProfitReport(&list,&iDTax);
+    getProfitReport(list,iDTax);
 
         return 0;
 }
