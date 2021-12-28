c65c357821c86049a219b0cfbcd3095d1bf8f33b
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2021-12-28 16:46:45 +0200

    Update ProfitCalculator.cpp

--- Pointers-References-DynamicMemory-Header Files/Profits1/ProfitCalculator.cpp
+++ Pointers-References-DynamicMemory-Header Files/Profits1/ProfitCalculator.cpp
@@ -4,31 +4,27 @@
 
 #include "Company.h"
 
-
-void getProfitReport(std::vector<Company> *list, std::map<int, int> *ptID_Tax)
+void getProfitReport(std::vector<Company>& list, std::map<int, int>& ID_Tax)
 {
-	std::map<int, int> iD_Tax = (*ptID_Tax);
+	//std::map<int, int> iD_Tax = (ptID_Tax);
 	int searchedID{ 0 };
 	int fulCosts{ 0 };
 
-	std::map<int, int>::iterator it = iD_Tax.begin();
-	for (unsigned  int i= 0; i < (*list).size(); i++)
+	std::map<int, int>::iterator it = ID_Tax.begin();
+	for (unsigned  int i= 0; i < (list).size(); i++)
 	{
-		searchedID=(*list)[i].getID();
-		it = iD_Tax.begin();  // resetting key in map on first possibility position
-		for (unsigned j = 0; j < (iD_Tax).size(); j++)
+		searchedID=(list)[i].getID();
+		it = ID_Tax.begin();  // resetting key in map on first possibility position
+		for (unsigned j = 0; j < (ID_Tax).size(); j++)
 		{
-
 			if (searchedID == (it->first))
 			{              //                   percentage %                  //    //        costs        //
-				fulCosts = ( ( (*list)[i].getRevenue() ) * ((it->second)*0.01) ) + ( (*list)[i].getCosts() );
+				fulCosts = ( ( (list)[i].getRevenue() ) * ((it->second)*0.01) ) + ( (list)[i].getCosts() );
 
-				std::cout << (*list)[i].getName() << " = " << ( ((*list)[i].getRevenue()) - fulCosts )<<'\n';
+				std::cout << (list)[i].getName() << " = " << ( ((list)[i].getRevenue()) - fulCosts )<<'\n';
 				break;
 			}
-
 			it++; // next key from map relevant on ID 
 		}
 	}
-	
 }
