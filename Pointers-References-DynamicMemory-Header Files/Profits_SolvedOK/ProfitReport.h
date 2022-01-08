79b7d569a61be99293ea91cfedea97942db87251
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-01-09 01:24:15 +0200

    Update ProfitReport.h

--- Pointers-References-DynamicMemory-Header Files/Profits_SolvedOK/ProfitReport.h
+++ Pointers-References-DynamicMemory-Header Files/Profits_SolvedOK/ProfitReport.h
@@ -1,38 +1,7 @@
 #ifndef ProfitReport_h
 #define ProfitReport_h
 #include <map>        
-#include <sstream>
 
-std::string getProfitReport(Company* fromInclusive, Company* toInclusive, std::map<int, ProfitCalculator> &profitCalculatorsByCompany)
-{
-	std::string something = "yes";
-	int profit{ 0 };
-	std::ostringstream stream;
-	std::string space = " = ";
-
-	unsigned int numberIteration{ profitCalculatorsByCompany.size() };
-	Company* ptr = fromInclusive;
-	while (numberIteration--)
-	{
-
-		std::map<int, ProfitCalculator>::iterator it= profitCalculatorsByCompany.begin();
-		for (size_t i=0; i < profitCalculatorsByCompany.size(); i++)
-		{
-			if (ptr->getId() == (it->first ))
-			{
-				profit = it->second.calculateProfit(*ptr);
-				stream << ptr->getName();
-				stream << space;
-				stream << profit;
-				stream << '\n';
-				break;
-			}
-			it++;
-		}
-		ptr++;
-	}
-
-	return stream.str();
-}
+std::string getProfitReport(Company* fromInclusive, Company* toInclusive, std::map<int, ProfitCalculator>& profitCalculatorsByCompany);
 
 #endif // !ProfitReport_h
