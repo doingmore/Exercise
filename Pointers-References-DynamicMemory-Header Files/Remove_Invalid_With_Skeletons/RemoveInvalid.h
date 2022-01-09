bca277b9042b46fa840a650b13864c20b41fb3fa
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-01-09 22:00:58 +0200

    Update RemoveInvalid.h

--- Pointers-References-DynamicMemory-Header Files/Remove_Invalid_With_Skeletons/RemoveInvalid.h
+++ Pointers-References-DynamicMemory-Header Files/Remove_Invalid_With_Skeletons/RemoveInvalid.h
@@ -3,26 +3,6 @@
 #include <list>
 #include "Company.h"
 
-void removeInvalid(std::list<Company*>& companies)
-{
-	unsigned int numberIteration = companies.size();
-	bool flagClearInvalid = true;
-	while (flagClearInvalid)
-	{
-		for (Company* ptr : companies)
-		{
-			flagClearInvalid = false;
-			if (ptr->getId() < 0 )
-			{
-				companies.remove(ptr);
-				delete ptr;
-				ptr = NULL;
-				flagClearInvalid = true;
-				break;
-			}
-		}
-	}
-}
-
+void removeInvalid(std::list<Company*>& companies);
 
 #endif // !RemoveInvalid
