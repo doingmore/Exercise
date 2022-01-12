4232ef2d78918f84fa53d1ea85244a3be9df67ff
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-01-12 08:48:57 +0200

    Add files via upload

---
+++ Pointers-References-DynamicMemory-Header Files/Duplicates_With_Skeletons/RemoveDuplicates.h
@@ -0,0 +1,8 @@
+#ifndef RemoveDuplicates_H
+#define RemoveDuplicates_H
+#include <list>
+#include "Company.h"
+
+void removeDuplicates(std::list<Company*>& companies);
+
+#endif // !RemoveDuplicates_H
