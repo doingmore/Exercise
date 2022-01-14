b62420da0a96cd50f727815c887ebfb3e026d515
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-01-14 10:49:01 +0200

    Add files via upload

---
+++ ClassMembersTemplatesCodeORG/SumOfVectors_WS/SumOfVectors.cpp
@@ -0,0 +1,10 @@
+#include "SumOfVectors.h"
+
+Vectors::operator+(const std::vector<std::string>& b)
+{
+}
+
+std::vector<std::string> operator+(const std::vector<string>& a, const std::vector<string>& b)
+{
+    return std::vector<std::string>();
+}
