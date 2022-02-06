b9a18d1937dc98152d29cf831cb16fbff64c95a2
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-02-06 12:06:17 +0200

    Add files via upload

---
+++ Destructors, Copy-Assignment, Construction/Register_WS/Register.cpp
@@ -0,0 +1,51 @@
+#include "Register.h"
+#include <string>
+
+Register::Register(size_t numCompanies)
+{
+	this->companiesArray = new Company [numCompanies];
+	numAdded=numCompanies;
+}
+
+void Register::add(const Company& c)
+{
+	int index{ 0 };
+	while (true)
+	{
+		if (companiesArray[index].getId() > 0)
+		{
+		}
+		else
+		{
+			companiesArray[index] = c;
+			break;
+		}
+		index++;
+	}
+}
+
+Company Register::get(int companyId) const
+{
+	for (size_t i = 0; i < this->numAdded; i++)
+	{
+		if (companyId == ( this->companiesArray[i].getId() ) )
+		{
+			return this->companiesArray[i];
+		}
+	}
+
+	return Company();
+}
+
+Register::~Register()
+{
+	delete[] companiesArray;
+}
+/*
+1
+2
+42 theanswer
+69 thehub
+69
+end
+*/
