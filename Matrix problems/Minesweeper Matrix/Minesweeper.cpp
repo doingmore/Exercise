25c31bfbcc55b58c20a5f097623f2547e3bbeaff
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-03-19 15:29:35 +0200

    Add files via upload

--- Matrix problems/Minesweeper Matrix/Minesweeper.cpp
+++ Matrix problems/Minesweeper Matrix/Minesweeper.cpp
@@ -1,116 +1,164 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <stack>
 
-using namespace std;
 
 /*
-You are given a N by M matrix (N and M are two integers entered on the console), in which the cells contain single characters
-– either a . (dot), or a ! (exclamation mark) – representing "empty" or "mined" positions. Write a program that prints a N by M matrix,
-where each cell contains a number, representing how many adjacent cells, including itself, are "mined". Each cell in a matrix
-has at most 8 adjacent cells – the cells directly above, below, to the left, to the right, as those diagonally – to the left and above,
-to the right and above, to the right and below and to the left and below.
+You are given a N by M matrix (N and M are two integers entered on the console), in which the cells contain single characters – either a . (dot),
+or a ! (exclamation mark) – representing "empty" or "mined" positions. Write a program that prints a N by M matrix, where each cell contains
+a number, representing how many adjacent cells, including itself, are "mined". Each cell in a matrix has at most 8 adjacent cells
+– the cells directly above, below, to the left, to the right, as those diagonally – to the left and above, to the right and above, to the right and below and to the left and below.
+
+		5 5
+		.....
+		...!.
+		.....
+		.....
+		.....
+ second test input
+		5 8
+		........
+		...!....
+		....!...
+		........
+		...!....
 */
 
-void readInput(vector<string>& outInput)
+void readMatrix(std::vector<std::string>& matrix)
 {
-    int rows{ 0 };
-    int cols{ 0 };
-    cin >> rows >> cols;
+	int row;
+	int col;
+	std::cin >> row >> col;
+	matrix.resize(row);
 
-    outInput.resize(rows);
-    for (size_t i = 0; i < rows; i++)
-    {
-        outInput[i].resize(cols);
-    }
-
-    for (size_t row = 0; row < rows; row++)
-    {
-        cin >> outInput[row];
-    }
+	for (size_t i = 0; i < row; i++)
+	{
+		matrix[i].resize(col);
+	}
 }
 
-void printMatrix(const vector<string>& matrix)
+void fillMatrix(std::vector<std::string>& matrix)
 {
-    for (size_t row = 0; row < matrix.size(); row++)
-    {
-        cout << matrix[row] << '\n';
-    }
+	char symbol;
+	for (size_t i = 0; i < matrix.size(); i++)
+	{
+		for (size_t k = 0; k < matrix[i].size(); k++)
+		{
+			std::cin >> symbol;
+			matrix[i][k]= symbol;
+		}
+
+	}
+	// transform '.' to zero '0'
+	for (size_t i = 0; i < matrix.size(); i++)
+	{
+		for (size_t k = 0; k < matrix[i].size(); k++)
+		{
+			if (matrix[i][k] == '.')
+			{
+				matrix[i][k] = '0';
+			}
+		}
+	}
+
 }
 
-void findMines(const vector<string>& matrix,
-    vector<int>& minesX,
-    vector<int>& minesY)
+bool findMine(int& r,int& c,std::vector<std::string>& matrix)
 {
-    for (size_t  row = 0; row < matrix.size(); row++)
-    {
-        for (size_t col = 0; col < matrix[row].size(); col++)
-        {
-            //found mine
-            if ('!' == matrix[row][col])
-            {
-                minesX.push_back(row);
-                minesY.push_back(col);
-            }
-        }
-    }
+	for (size_t i = 0; i < matrix.size(); i++)
+	{
+		for (size_t k = 0; k < matrix[i].size(); k++)
+		{
+			if (matrix[i][k] == '!')
+			{
+				r = i;
+				c = k; 
+				matrix[i][k] = 48;
+				return true;
+			}
+		}
+
+	}
+	return false;
 }
 
-void expandSingleMine(vector<string>& mineField,
-    const int ROW,
-    const int COL)
+void explodeMines(std::vector<std::string>& matrix)
 {
-    if (COL < 0 || ROW < 0 || COL >= (int)mineField[0].size()
-        || ROW >= (int)mineField.size())
-    {
-        return;
-    }
-    mineField[ROW][COL] += 1;
+	int coordinatRow{0};
+	int coordinatCol{0};
+	std::vector<std::pair<int, int> > coordinatesMines;
+
+	while (findMine(coordinatRow, coordinatCol, matrix))
+	{
+		matrix[coordinatRow][coordinatCol] +=1;
+		coordinatesMines.push_back(std::pair<int,int>(coordinatRow, coordinatCol));
+	}
+	
+	std::vector<std::pair<int, int> >::iterator it=coordinatesMines.begin();
+	for (size_t i = 0; i < coordinatesMines.size() ; i++, it++)
+	{
+		coordinatRow = it->first;
+		coordinatCol = it->second;
+		if ( coordinatCol + 1 < matrix[0].size()) // one right
+		{
+			matrix[coordinatRow][coordinatCol + 1] += 1;
+		}
+		if ( coordinatCol - 1 >= 0) // one left
+		{
+			matrix[coordinatRow][coordinatCol - 1] += 1;
+		}
+		if ( coordinatRow + 1 < matrix.size()) //one down
+		{
+			matrix[coordinatRow + 1][coordinatCol] += 1;
+		}
+		if ( coordinatRow - 1 >= 0) // one above
+		{
+			matrix[coordinatRow - 1][coordinatCol] += 1;
+		}
+		// diagonals
+		if (coordinatCol + 1 < matrix[0].size() && coordinatRow - 1 >= 0) // one right and above
+		{
+			matrix[coordinatRow - 1][coordinatCol + 1] += 1;
+		}
+		if (coordinatCol - 1 >= 0 && coordinatRow - 1 >= 0) // one left and above
+		{
+			matrix[coordinatRow - 1][coordinatCol - 1] += 1;
+		}
+		if (coordinatRow + 1 < matrix.size() && coordinatCol + 1 < matrix[0].size() ) // one right and down
+		{
+			matrix[coordinatRow+1][coordinatCol+1] += 1;
+		}
+		if (coordinatRow + 1 < matrix.size() && coordinatCol - 1 >= 0) // one left and down
+		{
+			matrix[coordinatRow + 1][coordinatCol - 1] += 1;
+		}
+	}
 }
 
-void expandMines(vector<string>& mineField,
-    const vector<int>& minesX,
-    const vector<int>& minesY)
+
+void printMatrix(std::vector<std::string>& matrix)
 {
-    for (size_t i = 0; i < minesX.size(); i++)
-    {
-        const int ROW = minesX[i];
-        const int COL = minesY[i];
-        //expand mine
-
-        expandSingleMine(mineField, ROW - 1, COL - 1);
-        expandSingleMine(mineField, ROW - 1, COL);
-        expandSingleMine(mineField, ROW - 1, COL + 1);
-
-        expandSingleMine(mineField, ROW, COL - 1);
-        expandSingleMine(mineField, ROW, COL);
-        expandSingleMine(mineField, ROW, COL + 1);
-
-        expandSingleMine(mineField, ROW + 1, COL - 1);
-        expandSingleMine(mineField, ROW + 1, COL);
-        expandSingleMine(mineField, ROW + 1, COL + 1);
-    }
+	for (size_t i = 0; i < matrix.size(); i++)
+	{
+		for (size_t k = 0; k < matrix[i].size(); k++)
+		{
+			std::cout << matrix[i][k];
+		}
+		std::cout << '\n';
+	}
 }
-
 
 int main()
 {
-    //1) read input
-    vector<string> mineField;
-    readInput(mineField);
+	std::vector<std::string> matrix;
+	
+	readMatrix(matrix);
+	
+	fillMatrix(matrix);
 
-    //2) find mines
-    vector<int> minesX;
-    vector<int> minesY;
-    findMines(mineField, minesX, minesY);
+	explodeMines(matrix);
 
-    vector<string> mineFieldValues(
-    mineField.size(), string(mineField[0].size(), '0'));
-
-    //3) expand mines
-    expandMines(mineFieldValues, minesX, minesY);
-    //4) print answer
-    printMatrix(mineFieldValues);
-
-    return 0;
+	printMatrix(matrix);
+	return 0;
 }
