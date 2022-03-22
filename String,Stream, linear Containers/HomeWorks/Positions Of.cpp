91ba524652f6019648e9c79f07c337fa43541418
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-03-22 10:40:03 +0200

    Add files via upload

---
+++ String,Stream, linear Containers/HomeWorks/Positions Of.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Write a program that reads a matrix of integers from the console, then a number, and prints all the positions at which that number appears in the matrix.
+The matrix definition on the console will contain a line with two positive integer numbers R and C – the number of rows and columns in the matrix – followed by
+R lines, each containing C numbers (separated by spaces), representing each row of the matrix.
+You should print each position on a single line – first print the row, then the column at which the number appears.
+If the number does not appear in the matrix, print not found
+*/
+
+void Problem2()
+{
+    int row{ 0 }, column{ 0 };
+    int matrix[100][100];
+    int wantedNumber{ 0 }, countSeccessfulMatch{ 0 };
+    cout << "How many rows are in a matrix: ";
+    cin >> row;
+    cout << "How many columns are in a matrix: ";
+    cin >> column;
+
+    matrix[row][column];
+
+    cout << "Input the numbers in matrix: " << '\n';
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Enter the wanted number:" << '\n';
+    cin >> wantedNumber;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            if (matrix[i][j] == wantedNumber)
+            {
+                cout << "row: " << i + 1 << " " << "column: " << j + 1 << '\n';
+                countSeccessfulMatch++;
+            }
+        }
+    }
+
+    if (countSeccessfulMatch == 0)
+    {
+        cout << "In this matrix not found wanted number" << '\n';
+    }
+}
---
+++ String,Stream, linear Containers/HomeWorks/The Signal and the Noise.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+/*
+Write a program which reads a sequence of non - negative integer numbers
+(in the decimal numeral system), separated by spaces, and finds the maximum number.
+The numbers in the input may have “noise” – symbols before, after, or between
+the digits of the number, which are not decimal digits(you need to ignore these
+when reading the numbers).All symbols in the input will be either letters, digits,
+punctuation, or signs from the standard ASCII table(no control symbols like newlines or tabs, delete, etc.)
+1.][.3 5--aA3:)5 2asd=@14
+*/
+
+void Problem6()
+{
+	string input;
+	getline(cin, input);
+	stringstream noise;
+	noise << input << '\n';
+	string fragment;
+	string strDigit;
+	string output;
+
+	int digit{ 0 };
+	int saveMaxDigit{ 0 };
+
+
+	while (noise)
+	{
+		noise >> fragment;
+		for (char character : fragment)
+		{
+			if (isdigit(character))
+			{
+				output = output + character;
+				strDigit = strDigit + character;
+			}
+		}
+		if (fragment != "")    // This chek is because in last iteration fragment is equal on "". This make a error in function stoi. 
+		{
+			digit = stoi(strDigit);
+		}
+		if (digit > saveMaxDigit)
+		{
+			saveMaxDigit = digit;
+		}
+		strDigit.clear();
+		fragment.clear();
+		output = output + " ";
+	}
+
+	cout << "The numbers are: " << output << " of which," << saveMaxDigit << " is the max." << '\n';
+}
