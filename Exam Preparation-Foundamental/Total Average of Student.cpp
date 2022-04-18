8600a3b97c434962677edfc1bda4a13e34f5f26f
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-04-18 18:14:53 +0300

    Add files via upload

---
+++ Exam Preparation-Foundamental/Total Average of Student.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+class Student
+{
+private:
+	std::string m_name;
+	std::string m_surname;
+	double m_totalAverage;
+public:
+	Student(std::string name, std::string surname, double evaulation) :
+		m_name{ name },
+		m_surname{ surname },
+		m_totalAverage{ evaulation }
+	{}
+
+	void print() const
+	{
+		std::cout << m_name << " " << m_surname << " -> " << m_totalAverage << '\n';
+	}
+
+	double getAverage() const { return m_totalAverage; }
+
+};
+
+int main_7()
+{
+	std::vector<Student> list;
+	int numberStudent{ 0 };
+	std::string name;
+	std::string surname;
+	double evaulation{ 0 };
+	std::cin >> numberStudent;
+	for (int i = 0; i < numberStudent; i++)
+	{
+		std::cin >> name >> surname >> evaulation;
+		list.emplace_back(Student{ name,surname,evaulation });
+	}
+	evaulation = 0;
+	for (Student s : list) // base range for loop
+	{
+		s.print();
+		evaulation += s.getAverage();
+	}
+
+	std::cout << evaulation / numberStudent << '\n'; // tatal average
+
+	return 0;
+}
