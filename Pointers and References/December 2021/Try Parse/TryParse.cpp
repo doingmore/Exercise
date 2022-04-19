3611790bc0e04284ea0c02b86f113f3a2340dc9b
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-04-19 20:52:39 +0300

    Add files via upload

---
+++ Pointers and References/December 2021/Try Parse/TryParse.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include "TryParse.h"
+
+void checkParse(std::string& first, std::string& second)
+{
+	std::string checkString = first;
+	int potentialNumber{ 0 };
+	int sum{ 0 };
+	bool flag = false;
+	
+	for (int i = 0; i < 2; i++)
+	{
+		for (unsigned int j = 0; j < checkString.size(); j++)
+		{
+			if (checkString[i] >= '0' && checkString[i] <= '9')
+			{
+				// OK do nothing
+			}
+			else //print and stop
+			{
+				if (i == 1)
+				{
+					std::cout << sum << " [Error] " << checkString << '\n';
+					flag = true;
+					break;
+				}
+				std::cout << "[Error] " << checkString << " " << second << '\n';
+				flag = true;
+				break;
+			}
+		}
+		if ((i==1) && (flag != true))  // successful parse finish
+		{
+			potentialNumber = stoi(checkString);
+			sum += potentialNumber;
+			std::cout << sum << '\n';
+			break;
+		}
+		else if (flag != true)
+		{
+		potentialNumber = stoi(checkString);
+		sum += potentialNumber;
+		checkString = second;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
