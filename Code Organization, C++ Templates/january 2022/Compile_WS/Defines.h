6656cd94a1a14dfa16acec0e19fcd799581bd5d3
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-05-16 10:21:32 +0300

    Add files via upload

---
+++ Code Organization, C++ Templates/january 2022/Compile_WS/Defines.h
@@ -0,0 +1,13 @@
+#ifndef Defines_H
+#define Defines_H
+
+#define STANDARD_TEMPLATE_LIBRARY namespace std;
+
+#define DONT_COMPILE_THIS !DONT_COMPILE_THIS
+
+#include <iostream>
+#include <string>
+
+
+
+#endif // !Defines_H
