f52c8d81b1cc28ff3c166c0de723f4525942ba1d
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-05-18 19:56:35 +0300

    Add files via upload

---
+++ Code Organization, C++ Templates/may 2022/Censorship.01/Article13Filter.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <set>
+#include <string>
+#include "Article13Filter.h"
+
+Article13Filter::Article13Filter(std::set<std::string> copyrighted) : copyrighted{ copyrighted }
+{
+}
+
+bool Article13Filter::blockIfCopyrighted(std::string s)
+{
+	if (copyrighted.count(s))
+	{
+		blocked.push_back(s);
+		return true;
+	}
+	return false;
+}
+
+bool Article13Filter::isCopyrighted(std::string s)
+{
+	if (copyrighted.count(s))
+	{
+		return true;
+	}
+	return false;
+}
+
+std::vector<std::string> Article13Filter::getBlocked()
+{
+	return blocked;
+}
+
