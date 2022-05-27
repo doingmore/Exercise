87aef9296d2d2020ba8540ca78b653925d2b962b
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-05-27 19:41:21 +0300

    Add files via upload

---
+++ Destructors,Copy-Assignment,Constructors/january 2022/Array of Pointers_WS/ArrayOfPointers.h
@@ -0,0 +1,20 @@
+#ifndef ArrayOfPointers_H
+#define ArrayOfPointers_H
+#include <vector>
+#include "Company.h"
+
+class ArrayOfPointers
+{
+private:
+	std::vector <Company*> arrayOfPtr;
+public:
+	ArrayOfPointers() {};
+
+	void add(Company*& c);
+	
+	size_t getSize();
+
+	Company* get(size_t& index);
+};
+
+#endif // !ArrayOfPointers_H
