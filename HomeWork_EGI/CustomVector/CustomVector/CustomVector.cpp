0593826bbdae44bbb95f8e4fdcbe0cb8c4a57dbe
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-07-14 19:02:53 +0300

    Update CustomVector.cpp

--- HomeWork_EGI/CustomVector/CustomVector/CustomVector.cpp
+++ HomeWork_EGI/CustomVector/CustomVector/CustomVector.cpp
@@ -10,17 +10,11 @@
 #define TestType Person
 
 
-
-
-
-
-
-
 Vector<TestType> otherScope()
 {
     Vector<TestType> answer;
 
-
+    
     Person first{"Mr.Robot",3};      // test for user defined type
     Person second{ "NONAME",8 };     // test for user defined type
     Person third{ "EXAMPLE",30 };    // test for user defined type
@@ -28,12 +22,13 @@ Vector<TestType> otherScope()
     answer.push_back(first);
     answer.push_back(second);
     answer.push_back(third);
+    
 
-
-
-    /*answer.push_back(5);            // test for int and char also
+    /*
+    answer.push_back(5);            // test for int and char also
     answer.push_back(8);              // test for int and char also
-    answer.push_back(10);*/           // test for int and char also
+    answer.push_back(10);           // test for int and char also
+    */
 
     /*answer.push_back("test");       // test for std::string
     answer.push_back("EGT");          // test for std::string
@@ -53,8 +48,8 @@ Vector<TestType> otherScope()
 int main()
 {
     // test memory leak
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
-    
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    /*
     // test copy CTR
     Vector<TestType> vec = otherScope();
     for (size_t i = 0; i < vec.size(); i++)
@@ -71,7 +66,41 @@ int main()
         std::cout << copy[i] << " ";
     }
     std::cout << '\n';
+    */
+    Vector<TestType> vec;
+    Person first{ "Mr.Robot",3 };      // test for user defined type
+    Person second{ "NONAME",8 };     // test for user defined type
+    Person third{ "EXAMPLE",30 };    // test for user defined type
 
+
+
+    std::vector<TestType> s_vec;
+    s_vec.push_back(first);
+    s_vec.resize(2);
+    for (size_t i = 0; i < s_vec.size(); i++)
+    {
+        std::cout << s_vec[i] << " ";
+    }
+
+    s_vec.push_back(second);
+    s_vec.push_back(third);
+    std::cout << s_vec.size() << " " << s_vec.capacity() << '\n';
+
+
+    vec.push_back(first);
+    vec.push_back(second);
+    vec.push_back(third);
+
+    std::cout << vec.size() << " " << vec.capacity();
+
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << '\n';
+
+    vec.erase();
+    
 
     return 0;
 }
