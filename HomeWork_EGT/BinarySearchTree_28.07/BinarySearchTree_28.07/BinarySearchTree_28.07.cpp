3f5d4667a95e286cd534b6b22af8e7883cd07f17
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-07-29 17:41:09 +0300

    Update BinarySearchTree_28.07.cpp

--- HomeWork_EGT/BinarySearchTree_28.07/BinarySearchTree_28.07/BinarySearchTree_28.07.cpp
+++ HomeWork_EGT/BinarySearchTree_28.07/BinarySearchTree_28.07/BinarySearchTree_28.07.cpp
@@ -5,10 +5,27 @@
 
 int main()
 {
-    BST tree;
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
 
-    tree.insert(5);
+    BST tree;
+    
     tree.insert(10);
+    tree.insert(16);
+    tree.insert(8);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(17);
+    tree.insert(5);
+    tree.insert(7);
+    
+    if (tree.search(17))
+    {
+        std::cout << "Succeeded find element" << '\n';
+    }
+    else
+    {
+        std::cout << "Not succeeded searching element" << '\n';
+    }
 
     return 0;
 }
