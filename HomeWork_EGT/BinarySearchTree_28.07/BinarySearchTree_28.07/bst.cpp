426d50b9950c3a7f5edc318aad3546686af27137
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-07-31 10:19:15 +0300

    Update bst.cpp

--- HomeWork_EGT/BinarySearchTree_28.07/BinarySearchTree_28.07/bst.cpp
+++ HomeWork_EGT/BinarySearchTree_28.07/BinarySearchTree_28.07/bst.cpp
@@ -2,6 +2,36 @@
 #include <queue>
 #include <iostream>
 
+//
+// ==================================================== Class Node =================================================
+// start
+
+bool BST::Node::isHaveNoChildren()
+{
+	if (this->getLeft() == NULL && this->getRight() == NULL)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool BST::Node::oneChild()
+{
+	if ( (this->getLeft() == NULL && this->getRight() != NULL) || 
+		 (this->getLeft() != NULL && this->getRight() == NULL) )
+	{
+		return true;
+	}
+	return false;
+}
+
+// end
+// ==================================================== Class Node =================================================
+//
+// 
+// ==================================================== Class BST ==================================================
+// start
+
 BST::BST(const int& rootValue)
 {
 	root = new Node{ rootValue };
@@ -70,19 +100,16 @@ bool BST::search(const int& element)
 		return false;
 	}
 	
-	
-	// return binary_search(root, element); // most power for BST ! ready
+	// return DFS_search(Node* node, const int& element);
 
-	// return DFS_search(root, element); // ready
 	std::queue<Node*> q;
 	return BFS_search(root, element,q);
 }
-
-bool BST::binary_search(Node* node, const int& element)
+BST::Node* BST::binary_search(Node* node, const int& element)
 {
 	if (node->getKey() == element)
 	{
-		return true;
+		return node;
 	}
 
 	if (element < node->getKey() )
@@ -93,21 +120,8 @@ bool BST::binary_search(Node* node, const int& element)
 	{
 		return binary_search(node->getRight(), element);
 	}
-}
 
-bool BST::DFS_search(Node* node, const int& element)
-{
-	if (node == NULL)
-	{
-		return false;
-	}
-	if (node->getKey() == element)
-	{
-		return true;
-	}
-
-	return ( DFS_search(node->getLeft(), element) ||
-		DFS_search(node->getRight(), element));
+	return NULL;
 }
 
 bool BST::BFS_search(Node* node, const int& element, std::queue<Node*>& q)
@@ -137,3 +151,158 @@ bool BST::BFS_search(Node* node, const int& element, std::queue<Node*>& q)
 	}
 	return false;
 }
+bool BST::DFS_search(Node* node, const int& element)
+{
+	if (node == NULL)
+	{
+		return false;
+	}
+	if (node->getKey() == element)
+	{
+		return true;
+	}
+
+	return (DFS_search(node->getLeft(), element) ||
+		DFS_search(node->getRight(), element));
+}
+
+BST::Node* BST::getParent(Node* parent, Node* node)
+{
+	if (parent == NULL)
+	{
+		return NULL; // for recursion
+	}
+	else if (parent == node)
+	{
+		return NULL;
+	}
+	else if(parent->getLeft() == node)
+	{
+		return parent;
+	}
+	else if (parent->getRight() == node)
+	{
+		return parent;
+	}
+	else if (node->getKey() < parent->getKey())
+	{
+		return getParent(parent->getLeft() , node);
+	}
+	else 
+	{
+		return getParent(parent->getRight(), node);
+	}
+	return NULL;
+}
+
+BST::Node* BST::getMinElement(Node* startNode)
+{
+	if (startNode->getLeft() != NULL)
+	{
+		return getMinElement(startNode->getLeft());
+	}
+	else
+	{
+		return startNode;
+	}
+}
+
+void BST::deleteElement(const int& element)
+{
+	Node* nodeForDel= binary_search(root,element);
+	if (nodeForDel == NULL)
+	{
+		// that element is not at that tree and do nothing
+		return;
+	}
+
+	if ( nodeForDel->isHaveNoChildren() ) // case 1 - no have children that mean that node is leaf
+	{
+		Node* parent = getParent(root, nodeForDel);
+		
+		if (parent == NULL) // if parent is NULL here ( no parent and no children ), that mean my node for delete is  the root ( for  tree ) 
+		{
+			root = NULL;
+		}
+		else if (parent->getLeft() == nodeForDel) // left child on parent
+		{
+			parent->setLeft(NULL);
+		}
+		else // right child
+		{
+			parent->setRight(NULL);
+		}
+	}
+	else if ( nodeForDel->oneChild() ) // case 2 - with one child
+	{
+		Node* parent = getParent(root,nodeForDel);
+		if (parent == NULL) // if parent is NULL here ( no parent ), but have one child that mean nodeForDelete is root and his child will become root on tree
+		{
+			if (nodeForDel->getLeft() == NULL) // have right child
+			{
+				root=nodeForDel->getRight();
+			}
+			else // have left child
+			{
+				root=nodeForDel->getLeft();
+			}
+		}
+		else if (parent->getLeft() == nodeForDel) // work with left child on parent
+		{
+			if (nodeForDel->getLeft() == NULL) // have right child
+			{
+				parent->setLeft( nodeForDel->getRight() );
+			}
+			else // have left child
+			{
+				parent->setLeft(nodeForDel->getLeft());
+			}
+		}
+		else // here work with parent right child
+		{
+			if (nodeForDel->getLeft() == NULL) // have right child
+			{
+				parent->setRight(nodeForDel->getRight());
+			}
+			else // have left child
+			{
+				parent->setRight(nodeForDel->getLeft());
+			}
+		}
+	}
+	else // case 3 - with two child
+	{
+		// quick recap key elements of this case
+		// most small element at binary search tree( or subtree ) is a node( with no children or one right children )
+		// when we delete element with two children we must replace his position with most small element from RIGHT subtree
+		// that case will use recursion to first or second case( delete node with no children and delete node with one children ) 
+		Node* minElement = getMinElement(nodeForDel->getRight()); // get minimum element from RIGHT subtree ! 
+		Node* parent = getParent(root,nodeForDel);
+
+		Node* newElement = new Node{ minElement->getKey() }; // get data from min Element
+		
+		this->deleteElement(minElement->getKey()); // delete min element 
+		minElement = NULL;
+		
+		newElement->setLeft(nodeForDel->getLeft());
+		newElement->setRight(nodeForDel->getRight());
+
+		if (parent == NULL ) // case when delete element is root for tree
+		{
+			root = newElement;
+		}
+		else if (parent->getLeft() == nodeForDel)
+		{
+			parent->setLeft(newElement);
+		}
+		else
+		{
+			parent->setRight(newElement);
+		}
+	}
+	delete nodeForDel;
+}
+
+// end
+// ==================================================== Class BST =================================================
+// 
