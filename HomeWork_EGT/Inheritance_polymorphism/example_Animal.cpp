bc56ed314b5c324c31cd0b73805c25facb6f1263
Svetozar Terziyski <89995481+doingmore@users.noreply.github.com>
2022-08-05 22:00:41 +0300

    Update example_Animal.cpp

--- HomeWork_EGT/Inheritance_polymorphism/example_Animal.cpp
+++ HomeWork_EGT/Inheritance_polymorphism/example_Animal.cpp
@@ -9,7 +9,14 @@ int main()
 {
     Cat cat{ "Fred" };
     Dog dog{ "Garbo" };
-    Pinscher tenProcent_Doberman{ "Skub" , true };
+    Pinscher tenProcent_Doberman{ "Skil" , true };
+
+    Animal* ptrC = new Cat{ cat };
+
+    delete ptrC;
+
+    tenProcent_Doberman.somethingNormalFunc();
+
 
     report(cat);
     report(dog);
